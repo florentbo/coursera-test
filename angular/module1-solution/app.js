81c84d02669f8c32dc4e325f4faf5587116fea68
Florent Bonamis <florent.bonamis.ext@proximus.com>
2016-10-05 14:41:17 +0200

    more DI code

--- angular/module1-solution/app.js
+++ angular/module1-solution/app.js
@@ -2,44 +2,46 @@
     'use strict';
 
     angular.module('LunchCheck', [])
-        .controller('LunchCheckController', function ($scope) {
-            $scope.messageContent = "";
-            $scope.messageStyle = "";
-            $scope.dishes = "";
+        .controller('LunchCheckController', LunchCheckController);
 
-            $scope.displayMessage = function () {
-                var message = chooseMessage($scope.dishes);
-                $scope.messageContent = message.content;
-                $scope.messageStyle = message.style;
-            };
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.messageContent = "";
+        $scope.messageStyle = "";
+        $scope.dishes = "";
 
-            function chooseMessage(dishes){
-                var comma = ',';
-                var enjoy = "Enjoy!";
-                var tooMuch = "Too much!";
-                var enterData = "Please enter data first";
-                var danger = "red";
-                var success = "green";
+        $scope.displayMessage = function () {
+            var message = chooseMessage($scope.dishes);
+            $scope.messageContent = message.content;
+            $scope.messageStyle = message.style;
+        };
 
-                if(!dishes){
-                    return {
-                        content : enterData,
-                        style : danger
-                    }
-                } else {
-                    var dishesList = dishes.split(comma);
-                    var filteredArray = [];
-                    angular.forEach(dishesList, function(item) {
-                        if (item.trim()) filteredArray.push(item);
-                    });
-                    var dishesListLength = filteredArray.length;
-                    return {
-                        content : dishesListLength >3 ? tooMuch : enjoy,
-                        style : success
-                    }
+        function chooseMessage(dishes) {
+            var comma = ',';
+            var enjoy = "Enjoy!";
+            var tooMuch = "Too much!";
+            var enterData = "Please enter data first";
+            var danger = "red";
+            var success = "green";
+
+            if (!dishes) {
+                return {
+                    content: enterData,
+                    style: danger
                 }
-
+            } else {
+                var dishesList = dishes.split(comma);
+                var filteredArray = [];
+                angular.forEach(dishesList, function (item) {
+                    if (item.trim()) filteredArray.push(item);
+                });
+                var dishesListLength = filteredArray.length;
+                return {
+                    content: dishesListLength > 3 ? tooMuch : enjoy,
+                    style: success
+                }
             }
-        });
 
+        }
+    }
 })();
