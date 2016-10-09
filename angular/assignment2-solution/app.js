be504fe1cad7d59f3074aa9edb332cf2768ef34f
florentbo <broche69@gmail.com>
2016-10-09 22:01:04 +0200

    angular module2 solution completed

--- angular/assignment2-solution/app.js
+++ angular/assignment2-solution/app.js
@@ -1,29 +1,6 @@
 (function () {
     'use strict';
 
-    var shoppingList = [
-        {
-            name: "Milk",
-            quantity: "2"
-        },
-        {
-            name: "Golf balls",
-            quantity: "77"
-        },
-        {
-            name: "Donuts",
-            quantity: "200"
-        },
-        {
-            name: "Cookies",
-            quantity: "300"
-        },
-        {
-            name: "Chocolate",
-            quantity: "5"
-        }
-    ];
-
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
@@ -56,19 +33,6 @@
         var toBuyItems = shoppingList;
         var boughtItems = [];
 
-        /*service.addItem = function (itemName, quantity) {
-            var item = {
-                name: itemName,
-                quantity: quantity
-            };
-            items.push(item);
-        };
-
-        service.removeItem = function (itemIdex) {
-            items.splice(itemIdex, 1);
-        };*/
-
-
         service.moveItem = function (itemIndex) {
             var item = toBuyItems[itemIndex];
             toBuyItems.splice(itemIndex, 1);
@@ -83,4 +47,28 @@
             return boughtItems;
         };
     }
+
+    var shoppingList = [
+        {
+            name: "Milk",
+            quantity: "2"
+        },
+        {
+            name: "Golf balls",
+            quantity: "77"
+        },
+        {
+            name: "Donuts",
+            quantity: "200"
+        },
+        {
+            name: "Cookies",
+            quantity: "300"
+        },
+        {
+            name: "Chocolate",
+            quantity: "5"
+        }
+    ];
+
 })();
